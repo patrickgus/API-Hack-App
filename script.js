eaa1a946590ebc6704177708ee86be7b1d5bb8f3
Patrick Gustafson <patrickgus@charter.net>
2019-10-08 15:31:29 -0700

    added functions to script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const apiKey = '52c6dff4ea5e83aba69fe752999caa2c';
+
+function formatQueryParams {
+  // format the query parameters to make API call
+
+}
+
+function displayResults {
+  // displays the results of the search with links to the artist or song
+
+}
+
+function getArtistsOrSongs {
+  // uses API to get the name of the artist or song 
+  // displays nothing if no results are found
+
+}
+
+function watchForm {
+  // listens for click on submit button
+
+}
+
+$(watchForm);
