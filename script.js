84df7b6616a411fa1a5f8fbf53bf95d0ed7e6df9
Patrick Gustafson <patrickgus@charter.net>
2019-10-11 11:12:18 -0700

    add watchForm function to listen for search term submit

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
 'use strict';
 
 const apiKey = '52c6dff4ea5e83aba69fe752999caa2c';
+const lastFmSearchUrl = 'http://ws.audioscrobbler.com/2.0/';
+const lyricsSearchUrl = 'https://api.lyrics.ovh/v1/';
 
 function formatQueryParams {
   // format the query parameters to make API call
 
 }
 
-function displayResults {
+function displaySearchResults {
   // displays the results of the search with links to the artist or song
   // displays "No results found, Please try another search" if no results are found
 
 }
 
-function getArtistsOrSongs {
+function getArtistOrSongs {
   // uses API to get the name of the artist or song 
 
 }
 
 function watchForm {
-  // listens for click on submit button
+  $('form').submit(event => {
+    event.preventDefault();
 
+    const searchTerm = $('#js-search-term').val();
+
+    getArtistOrSongs(searchTerm);
+  });
 }
 
 $(watchForm);
